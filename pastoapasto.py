57d0eac93fff03fada6cab517b735f0dc9b234e1
acsarda <sardaugusto@gmail.com>
2020-07-15 15:10:16 -0300

    - agregamos favicon - agregamos navegación a la pantalla paso2

--- pastoapasto.py
+++ pastoapasto.py
@@ -1,8 +1,7 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, session, redirect, url_for
 from flask_wtf import FlaskForm
 from wtforms import StringField, DateField, SubmitField
 from wtforms.validators import DataRequired
-
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'hard to guess key'
@@ -11,10 +10,20 @@ class PantallaPasoUno(FlaskForm):
     fecha = DateField(validators=[DataRequired()])
     establecimiento = StringField(validators=[DataRequired()])
     localidad = StringField(validators=[DataRequired()])
-
+    siguiente = SubmitField("Siguiente")
 
 @app.route('/index', methods = ['GET', 'POST'])
 def index():
-    form = PantallaPasoUno()
+	form = PantallaPasoUno()
 
-    return render_template('index.html', form=form)
+	if request.method=='POST' and form.validate():
+		session['fecha']=form.fecha.data
+		session['establecimiento']=form.establecimiento.data
+		session['localidad']=form.localidad.data
+		return redirect(url_for('paso2'))
+	return render_template('index.html', form=form)
+	
+@app.route('/paso2')
+def paso2():
+	return render_template('paso2.html')
+# q
